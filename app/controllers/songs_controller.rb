b6cab8919ef225e113f35d1ee7b9807252d493f4
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-10-01 23:32:44 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -49,7 +49,7 @@ class SongsController < ApplicationController
         redirect_to artists_path, alert: "Artist not found."
       else
         @song = artist.songs.find_by(id: params[:id])
-        redirect_to artist_songs_path(artist), alert: "Song not found" if @song.nil
+        redirect_to artist_songs_path(artist), alert: "Song not found" if @song.nil?
       end
     else
       @song = Song.find(params[:id])
@@ -81,4 +81,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
-
