53510db06d855a686431416704dd7a6ddfd76c0b
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-10-01 23:25:35 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -12,10 +12,10 @@ class Song < ActiveRecord::Base
 
   def artist_select(song)
     if song.artist.nil?
-      select_tag "song[artist_id]",
+      select_tag "post[author_id]",
       options_from_collection_for_select(Artist.all, :id, :name)
     else
-      hidden_field_tag "song[artist_id]", song.artist_id
+      hidden_field_tag "post[author_id]"
     end
   end    
 end
