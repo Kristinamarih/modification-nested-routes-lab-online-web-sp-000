bffdeca649dee6e0a5e834916dcbe2afdae5bfd4
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-10-02 00:08:52 +0000

    Automatically backed up by Learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -4,11 +4,11 @@ module ArtistsHelper
   end
 
   def artist_select(song, nested)
-    if song.artist.nil?
+    if nested
+      hidden_field_tag "song[artist_id]"
+    else
       select_tag "song[artist_id]",
       options_from_collection_for_select(Artist.all, :id, :name)
-    else
-      hidden_field_tag "song[artist_id]"
     end
   end
 end
